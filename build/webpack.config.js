a365e65529215296974a9ac3a1fa576437affcf2
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2018-02-24 21:39:48 +0200

    Support for prod builds (aot not working still)

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -13,7 +13,6 @@ var ionicWebpackFactory = require(process.env.IONIC_WEBPACK_FACTORY);
 var deepExtend = require('deep-extend');
 var cordovaLib = require('cordova').cordova_lib;
 
-var ModuleConcatPlugin = require('webpack/lib/optimize/ModuleConcatenationPlugin');
 var PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
 
 var CSON = require('cson');
@@ -22,140 +21,130 @@ var configOverwrite = CSON.requireFile('./config/config.cson');
 
 const RawConfig = deepExtend(defaultConfig, configOverwrite);
 
-var optimizedProdLoaders = [
-  {
-    test: /\.json$/,
-    loader: 'json-loader'
-  },
-  {
-    test: /\.cson$/,
-    use: [
-      `file-loader?name=i18n/[name].json&publicPath=i18n&outputPath=${process.env.IONIC_WWW_DIR}&useRelativePath=true`,
-      'strip-module-export-loader',
-      'cson-loader'
+function getOptimizedProdLoaders() {
+  var optimizedProdLoaders = [
+    {
+      test: /\.json$/,
+      loader: 'json-loader'
+    },
+    {
+      test: /\.cson$/,
+      use: [
+        `file-loader?name=i18n/[name].json&publicPath=i18n&outputPath=${process.env.IONIC_WWW_DIR}&useRelativePath=true`,
+        'strip-module-export-loader',
+        'cson-loader'
+      ],
+      include: path.join(__dirname, '..', 'src', 'i18n')
+    },
+    {
+      test: /\.cson$/,
+      use: 'cson-loader',
+      exclude: path.join(__dirname, '..', 'src', 'i18n')
+    },
+    {
+      test: /\.js$/,
+      loader: [
+        {
+          loader: process.env.IONIC_CACHE_LOADER
+        }
+      ]
+    },
+    {
+      test: /\.ts$/,
+      loader: [
+        {
+          loader: process.env.IONIC_CACHE_LOADER
+        },
+        {
+          loader: process.env.IONIC_WEBPACK_LOADER
+        }
+      ]
+    }
+  ];
+  return optimizedProdLoaders;
+}
+
+function getBasicConfig() {
+  var basicConfig = {
+    entry: ['babel-polyfill', process.env.IONIC_APP_ENTRY_POINT],
+    output: {
+      path: '{{BUILD}}',
+      publicPath: 'build/',
+      filename: '[name].js',
+      devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
+    },
+    devtool: process.env.IONIC_SOURCE_MAP_TYPE,
+
+    resolve: {
+      extensions: ['.ts', '.js', '.json', '.cson'],
+      modules: [path.resolve('node_modules')]
+    },
+
+    module: {
+      loaders: [
+        {
+          test: /\.json$/,
+          loader: 'json-loader'
+        },
+        {
+          test: /\.cson$/,
+          use: [
+            `file-loader?name=i18n/[name].json&publicPath=i18n&outputPath=${process.env.IONIC_WWW_DIR}&useRelativePath=true`,
+            'strip-module-export-loader',
+            'cson-loader'
+          ],
+          include: path.join(__dirname, '..', 'src', 'i18n')
+        },
+        {
+          test: /\.cson$/,
+          use: 'cson-loader',
+          exclude: path.join(__dirname, '..', 'src', 'i18n')
+        },
+        {
+          test: /\.ts$/,
+          loader: process.env.IONIC_WEBPACK_LOADER
+        }
+      ]
+    },
+
+    plugins: [
+      ionicWebpackFactory.getIonicEnvironmentPlugin(),
+      ionicWebpackFactory.getCommonChunksPlugin(),
+      new webpack.DefinePlugin({
+        __VERSION__: JSON.stringify(getAppVersion()),
+        __DEV__: process.env.HYBRID_ENV === 'dev',
+        __PROD__: process.env.HYBRID_ENV === 'prod',
+        __SW_ENABLED__: RawConfig.serviceWorker.enabled,
+        __PERMALINKS_POST__: RawConfig.permalinks.post,
+        __PERMALINKS_TAG__: RawConfig.permalinks.tag,
+        __PERMALINKS_CATEGORY__: RawConfig.permalinks.category,
+        __PERMALINKS_AUTHOR__: RawConfig.permalinks.author,
+        __CONFIG_FOLDER__: JSON.stringify(process.env.IONIC_ROOT_DIR + '/config'),
+      }),
     ],
-    include: path.join(__dirname, '..', 'src', 'i18n')
-  },
-  {
-    test: /\.cson$/,
-    use: 'cson-loader',
-    exclude: path.join(__dirname, '..', 'src', 'i18n')
-  },
-  {
-    test: /\.js$/,
-    loader: [
-      {
-        loader: process.env.IONIC_CACHE_LOADER
-      },
 
-      {
-        loader: '@angular-devkit/build-optimizer/webpack-loader',
-        options: {
-          sourceMap: true //process.env.IONIC_GENERATE_SOURCE_MAP
-        }
-      }
-    ]
-  },
-  {
-    test: /\.ts$/,
-    loader: [
-      {
-        loader: process.env.IONIC_CACHE_LOADER
-      },
-
-      {
-        loader: '@angular-devkit/build-optimizer/webpack-loader',
-        options: {
-          sourceMap: true //process.env.IONIC_GENERATE_SOURCE_MAP
-        }
-      },
-
-      {
-        loader: process.env.IONIC_WEBPACK_LOADER
-      }
-    ]
-  }
-];
-
-var basicConfig = {
-  entry: process.env.IONIC_APP_ENTRY_POINT,
-  output: {
-    path: '{{BUILD}}',
-    publicPath: 'build/',
-    filename: '[name].js',
-    devtoolModuleFilenameTemplate: ionicWebpackFactory.getSourceMapperFunction(),
-  },
-  devtool: process.env.IONIC_SOURCE_MAP_TYPE,
-
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-    modules: [path.resolve('node_modules')]
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        test: /\.cson$/,
-        use: [
-          `file-loader?name=i18n/[name].json&publicPath=i18n&outputPath=${process.env.IONIC_WWW_DIR}&useRelativePath=true`,
-          'strip-module-export-loader',
-          'cson-loader'
-        ],
-        include: path.join(__dirname, '..', 'src', 'i18n')
-      },
-      {
-        test: /\.cson$/,
-        use: 'cson-loader',
-        exclude: path.join(__dirname, '..', 'src', 'i18n')
-      },
-      {
-        test: /\.ts$/,
-        loader: process.env.IONIC_WEBPACK_LOADER
-      }
-    ]
-  },
-
-  plugins: [
-    ionicWebpackFactory.getIonicEnvironmentPlugin(),
-    ionicWebpackFactory.getCommonChunksPlugin(),
-    new webpack.DefinePlugin({
-      __VERSION__: JSON.stringify(getAppVersion()),
-      __DEV__: process.env.IONIC_ENV === 'dev',
-      __PROD__: process.env.IONIC_ENV === 'prod',
-      __SW_ENABLED__: RawConfig.serviceWorker.enabled,
-      __PERMALINKS_POST__: RawConfig.permalinks.post,
-      __PERMALINKS_TAG__: RawConfig.permalinks.tag,
-      __PERMALINKS_CATEGORY__: RawConfig.permalinks.category,
-      __PERMALINKS_AUTHOR__: RawConfig.permalinks.author,
-      __CONFIG_FOLDER__: JSON.stringify(process.env.IONIC_ROOT_DIR + '/config'),
-    }),
-  ],
-
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
-  }
-};
-
-function getDevConfig() {
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
+    }
+  };
   return basicConfig;
 }
 
+function getDevConfig() {
+  return getBasicConfig();
+}
+
 function getProdConfig() {
-  var config = Object.assign({}, basicConfig);
+  var config = Object.assign({}, getBasicConfig());
 
   if (process.env.IONIC_OPTIMIZE_JS === 'true') {
-    config.module.loaders = optimizedProdLoaders;
+    config.module.loaders = getOptimizedProdLoaders();
   }
   config.plugins.push(...[
-      new ModuleConcatPlugin(),
       new PurifyPlugin()
   ]);
 
@@ -167,8 +156,19 @@ function getAppVersion() {
   return config.version();
 }
 
+var config;
+if (process.env.HYBRID_ENV && process.env.HYBRID_ENV === 'prod') {
+  config = getProdConfig();
+}
+else {
+  process.env.HYBRID_ENV = 'dev';
+  config = getDevConfig();
+}
+
+console.log(JSON.stringify(process.env, null, 2));
+
 module.exports = {
-  dev: getDevConfig(),
-  prod: getProdConfig()
+  dev: config,
+  prod: config
 }
 
