02a26a7cb0bbeaee0959d59b5c2262fe4ea1c40e
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2018-02-25 02:57:15 +0200

    Normalize fix

--- build/sass.config.js
+++ build/sass.config.js
@@ -1,6 +1,7 @@
 let config = require('./../node_modules/@ionic/app-scripts/config/sass.config.js');
 config.sourceMap = true;
 config.variableSassFiles = [
+  '{{SRC}}/theme/normalize.scss',
   '{{SRC}}/theme/variables.scss',
   'config/theme/variables.scss'
 ];
