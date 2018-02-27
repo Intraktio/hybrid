f56be2047c8bb840a2d601af9ce593711579f64e
Karri Rasinmäki <karri.rasinmaki@gmail.com>
2018-02-27 05:12:54 +0200

    Build script with purify css

---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PATH=$PATH:$(npm bin)
+set -x
+ 
+BUILDFOLDER=www/
+ 
+# clean up previous build
+rm -fr $BUILDFOLDER
+ 
+# Prod build
+HYBRID_ENV=prod
+ionic-app-scripts build --no-minifyJs --minifyCss --optimizeJs \
+# ionic-app-scripts build --prod \
+                        --wwwDir $BUILDFOLDER
+ 
+# remove unused css
+purifycss $BUILDFOLDER"build/main.css" \
+          $BUILDFOLDER"build/*.js" \
+          --info \
+          --min \
+          --out $BUILDFOLDER"build/main.css" \
+          #--whitelist .bar-button-default
